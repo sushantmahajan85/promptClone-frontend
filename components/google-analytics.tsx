04dd251d997d28bc248a2575f664dc5def3600e1
DhruvGodambe <dhruvrockstone@gmail.com>
2026-05-03 19:21:30 +0530

    google analytics added

---
+++ components/google-analytics.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+declare global {
+  var gtag: ((...args: unknown[]) => void) | undefined;
+}
+
+function trackPageView(url: string) {
+  if (!globalThis.gtag || !GA_MEASUREMENT_ID) return;
+
+  globalThis.gtag("config", GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+}
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!GA_MEASUREMENT_ID) return;
+
+    const query = searchParams?.toString();
+    const url = query ? `${pathname}?${query}` : pathname;
+    trackPageView(url);
+  }, [pathname, searchParams]);
+
+  if (!GA_MEASUREMENT_ID) return null;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', { send_page_view: false });
+        `}
+      </Script>
+    </>
+  );
+}
